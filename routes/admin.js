48ee8e507f4a76282f21b57c409a993b71367919
German <georg.88@mail.ru>
2017-09-04 13:20:23 +0700

    add login page

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/login', (req, res) => {
+  res.render('login', {layout: false});
+});
+
 router.get('*', function (req, res) {
-   res.render('admin', {layout: false});
+  res.render('admin', {layout: false});
 });
 
 module.exports = router;
